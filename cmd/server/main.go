3ff003fe103e4db1f21a29e5f8193f3e2db260e5
Nikolay Ivanov <nikolay9619962@mail.ru>
2025-10-05 22:06:50 +0300

    global vars

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,11 +2,20 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/Nikolay961996/metsys/internal/server"
 	"github.com/Nikolay961996/metsys/models"
 )
 
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
+
 func main() {
+	printHello()
 	err := models.Initialize("info")
 	if err != nil {
 		panic(err)
@@ -17,4 +26,17 @@ func main() {
 	entity := server.InitServer(&c)
 	entity.Run(c.RunOnServerAddress, c.KeyForSigning)
 	defer entity.Stop()
+}
+
+func printHello() {
+	fmt.Printf("Build version: %s\n", ifNA(buildVersion))
+	fmt.Printf("Build date: %s\n", ifNA(buildDate))
+	fmt.Printf("Build commit: %s\n", ifNA(buildCommit))
+}
+
+func ifNA(s string) string {
+	if s == "" {
+		return "N/A"
+	}
+	return s
 }
