9a76b207e0991b950572d9da1e173d7595ff6d8a
Nikolay Ivanov <nikolay9619962@mail.ru>
2025-10-19 20:17:46 +0300

    fmt

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,8 +2,14 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"net/http"
 	_ "net/http/pprof"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/Nikolay961996/metsys/internal/agent"
 	"github.com/Nikolay961996/metsys/internal/buildinfo"
@@ -27,10 +33,30 @@ func main() {
 	a := agent.InitAgent()
 	go a.Run(&c)
 
-	defer a.Stop()
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 
-	err = http.ListenAndServe(addr, nil)
-	if err != nil {
+	srv := runBackground()
+	gracefulShutdown(&a, srv, sigCh)
+}
+
+func runBackground() *http.Server {
+	srv := &http.Server{Addr: addr}
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			panic(err)
+		}
+	}()
+	return srv
+}
+
+func gracefulShutdown(a *agent.Entity, srv *http.Server, sigCh <-chan os.Signal) {
+	<-sigCh
+	a.Stop()
+	time.Sleep(2 * time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
 		panic(err)
 	}
 }
